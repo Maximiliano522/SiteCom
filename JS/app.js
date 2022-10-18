6a9336eac7f019d9214ac0510c9a6e38ad7ff0be
Maximiliano522 <josemaximilianoep@hotmail.com>
2022-10-18 16:31:25 -0500

    Actualizacion

--- JS/app.js
+++ JS/app.js
@@ -1,21 +1,24 @@
 const productData = [
     {
         index: '01',
-        name: 'yellow tracksuit',  
-        image: 'img1.jpg', 
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium pariatur odio aspernatur, dignissimos modi harum.', 
+        name: 'Blue york',  
+        image: './img/img1.jpg', 
+        des: 'Los grandes diseñadores de moda de Nueva York, París y Milán, traen lo nuevo a la industria de la ropa, con sus colecciones de colores vibrantes, outfits monocromáticos, y estampados.', 
+        desLeft: 'Gran y nuevo diseño de los diseñadores de la marca Nueva York',
     },
     {
         index: '02',
-        name: 'designer outfit',  
-        image: 'img2.jpg', 
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium pariatur odio aspernatur, dignissimos modi harum.', 
+        name: 'Red outfit',  
+        image: './img/img2.jpg', 
+        des: 'Alerta peligro. Y, sobre todo, es el color de la consciencia. La mujer que elige llevar un vestido rojo hace una afirmación imponente: sabe quién es y sabe perfectamente cuál es su lugar.', 
+        desLeft: 'Gran y nuevo diseño de los diseñadores de la marca New Mod',
     },
     {
         index: '03',
-        name: 'chilling mood',  
-        image: 'img3.jpg', 
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium pariatur odio aspernatur, dignissimos modi harum.', 
+        name: 'Green Mood',  
+        image: './img/img3.jpg', 
+        des: '¿Qué significa el vestido color Verde: frescura, libertad, sanación, esperanza, bienestar y serenidad todo lo positivo se asocia al verde al igual que la felicidad. Con el color verde se avanza, se progresa y se logra ser más positivo',
+        desLeft: 'Gran y nuevo diseño de los diseñadores de la marca Shein', 
     }
 ]
 
@@ -23,7 +26,7 @@ const nxtBtn = document.querySelector('.nxt-btn')
 let smImgContainer = document.querySelector('.sm-product-img-container')
 let smImg = document.querySelector('.sm-product-img')
 let productIndex = document.querySelector('.product-index')
-let smProductDes = document.querySelector('.sm-query-des')
+let smProductDes = document.querySelector('.sm-product-des')
 
 let productImgContainer = document.querySelector('.product-img-container')
 let productImg = document.querySelector('.product-img')
@@ -42,5 +45,10 @@ nxtBtn.addEventListener('click', () => {
         currentProduct ++
     }
     productIndex.innerHTML = productData[currentProduct].index
-    smProductDes.innerHTML = productData[currentProduct].des.slice(0,80)
+    smProductDes.innerHTML = productData[currentProduct].desLeft.slice(0,100)
+    productDes.innerHTML = productData[currentProduct].des.slice(0,200)
+    productName.innerHTML = productData[currentProduct].name
+    smImg.src= productData[currentProduct].image;
+    backdropImg.src= productData[currentProduct].image;
+    productImg.src= productData[currentProduct].image;
 })
